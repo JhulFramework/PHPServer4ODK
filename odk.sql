5e309fc45dabd939fd9015a38a62a30d56bc0007
manish dhruw <eskylite@gmail.com>
2016-11-27 09:03:39 +0530

    updated database design

--- odk.sql
+++ odk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 21, 2016 at 02:53 PM
+-- Generation Time: Nov 26, 2016 at 01:33 PM
 -- Server version: 10.0.27-MariaDB
 -- PHP Version: 5.5.14
 
@@ -23,25 +23,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `submitted_forms`
+-- Table structure for table `submitted_data`
 --
 
-CREATE TABLE IF NOT EXISTS `submitted_forms` (
+CREATE TABLE IF NOT EXISTS `submitted_data` (
   `ik` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `year` varchar(6) NOT NULL,
   `month` int(2) NOT NULL,
   `day` int(2) NOT NULL,
   `created` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `submitted_forms_data`
+-- Table structure for table `submitted_data_content`
 --
 
-CREATE TABLE IF NOT EXISTS `submitted_forms_data` (
+CREATE TABLE IF NOT EXISTS `submitted_data_content` (
   `ik` int(11) NOT NULL,
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -75,23 +75,24 @@ INSERT INTO `users` (`ik`, `access`, `iname`, `password`) VALUES
 CREATE TABLE IF NOT EXISTS `xforms` (
   `ik` int(11) NOT NULL,
   `name` varchar(99) NOT NULL,
+  `md5` varchar(32) NOT NULL,
   `r_url` varchar(200) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `submitted_forms`
+-- Indexes for table `submitted_data`
 --
-ALTER TABLE `submitted_forms`
+ALTER TABLE `submitted_data`
   ADD PRIMARY KEY (`ik`);
 
 --
--- Indexes for table `submitted_forms_data`
+-- Indexes for table `submitted_data_content`
 --
-ALTER TABLE `submitted_forms_data`
+ALTER TABLE `submitted_data_content`
   ADD PRIMARY KEY (`ik`);
 
 --
@@ -111,10 +112,10 @@ ALTER TABLE `xforms`
 --
 
 --
--- AUTO_INCREMENT for table `submitted_forms`
+-- AUTO_INCREMENT for table `submitted_data`
 --
-ALTER TABLE `submitted_forms`
-  MODIFY `ik` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+ALTER TABLE `submitted_data`
+  MODIFY `ik` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -124,7 +125,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `xforms`
 --
 ALTER TABLE `xforms`
-  MODIFY `ik` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `ik` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
